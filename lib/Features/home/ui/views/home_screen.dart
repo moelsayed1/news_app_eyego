19ee01de84ac86be6fcdb0f73e3f2904fe2cc401
Mostafa Elsayed <mostafaembaby19@gmail.Com>
2025-04-09 05:43:19 +0200

    - finish my app

--- lib/Features/home/ui/views/home_screen.dart
+++ lib/Features/home/ui/views/home_screen.dart
@@ -1,38 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:news_app_eyego/Core/controllers/drawer_controller.dart';
 import 'package:news_app_eyego/Core/helpers/spacing.dart';
-import 'package:news_app_eyego/Core/widgets/custom_drawer.dart';
-import 'package:news_app_eyego/Features/home/ui/views/widgets/categories_list_view.dart';
-import 'package:news_app_eyego/Features/home/ui/views/widgets/news_list_view_builder.dart';
-import 'package:news_app_eyego/Features/search/ui/search_screen.dart';
-
 
 class HomeScreen extends StatelessWidget {
-   HomeScreen({super.key});
-
-  final AppDrawerController drawerController = Get.put(AppDrawerController());
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: drawerController.scaffoldKey,
       appBar: AppBar(
         centerTitle: true,
-        leading: IconButton(
-          onPressed: drawerController.openDrawer,
-          icon: const Icon(Icons.menu),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Get.to(() => SearchScreen());
-            },
-            icon: const Icon(Icons.search),
-          ),
-          horizontalSpace(10),
-        ],
         title: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -56,23 +33,12 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      drawer: CustomDrawer(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 22.h),
-        child: CustomScrollView(
-          physics: BouncingScrollPhysics(),
-          scrollDirection: Axis.vertical,
-          slivers: [
-            SliverToBoxAdapter(
-              child: CategoriesListView(),
-            ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: 20.h,
-              ),
-            ),
-            NewsListViewBuilder(category: 'general'),
-          ],
+      body: Center(
+        child: Text(
+          'Welcome to the Home Page',
+          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+            color: Colors.black,
+          ),
         ),
       ),
     );
