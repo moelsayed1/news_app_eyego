f7afaaec12a9ef22e9b537d58ef898cb2597fef7
Mostafa Elsayed <mostafaembaby19@gmail.Com>
2025-04-12 21:59:50 +0200

    - create controllers

---
+++ lib/Core/controllers/news_controller.dart
@@ -0,0 +1,74 @@
+import 'dart:developer';
+
+import 'package:get/get.dart';
+import 'package:dio/dio.dart' as ApiFetcher;
+import 'package:news_app_eyego/Features/home/data/models/article_model.dart';
+
+class newsController extends GetxController {
+  List<ArticleModel> articles = [];
+  List<String> categories = [
+    'general',
+    'business',
+    'entertainment',
+    'health',
+    'science',
+    'sports',
+    'technology'
+  ];
+  String selectedCategory = 'technology';
+  bool isLoading = true;
+
+  @override
+  void onInit() {
+    //Activate Dio
+    getArticles();
+
+    super.onInit();
+  }
+
+  onCategorySelected(String category) {
+    isLoading = true;
+    selectedCategory = category;
+    articles.clear();
+    getArticles();
+    isLoading = false;
+    update();
+  }
+
+  Future getArticles() async {
+    isLoading = true;
+    ApiFetcher.Response response = await ApiFetcher.Dio().get(
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=7d029235a3a54779ac27799eca66fa36&category=$selectedCategory');
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> article2 = jsonData['articles'];
+
+    for (var article in article2) {
+      ArticleModel articleModel = ArticleModel.fromJson(article);
+      articles.add(articleModel);
+    }
+    isLoading = false;
+    update();
+  }
+
+  static Future<List<ArticleModel>> getArticlesBySearch(
+      {required String query}) async {
+    try {
+
+      ApiFetcher.Response response = await ApiFetcher.Dio().get(
+          'https://newsapi.org/v2/everything?q=$query&apiKey=7d029235a3a54779ac27799eca66fa36');
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articleData = jsonData['articles'];
+      List<ArticleModel> articles = [];
+      for (var article in articleData) {
+        if (article['title'] == null) continue;
+        ArticleModel articleModel = ArticleModel.fromJson(article);
+        articles.add(articleModel);
+        log("added an article ${articles.length}");
+      }
+
+      return articles;
+    } on Exception {
+      return [];
+    }
+  }
+}
